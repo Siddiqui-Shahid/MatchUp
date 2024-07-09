9ef34d37944ece8247156cc457d8e4b9a2bcdb7c
Siddiqui-Shahid <136562373+Shahid-Siddiqui-Raw@users.noreply.github.com>
2024-07-09 13:29:51 +0530

    Initial project made

---
+++ MatchUp/Build/Intermediates.noindex/Previews/MatchUp/Intermediates.noindex/MatchUp.build/Debug-iphonesimulator/MatchUp.build/DerivedSources/CoreDataGenerated/MatchUp/MatchUp+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  MatchUp+CoreDataModel.swift
+//  
+//
+//  Created by Muhammed Siddiqui on 7/9/24.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
---
+++ MatchUp/MatchUp/MatchUpApp.swift
@@ -0,0 +1,20 @@
+//
+//  MatchUpApp.swift
+//  MatchUp
+//
+//  Created by Muhammed Siddiqui on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchUpApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
